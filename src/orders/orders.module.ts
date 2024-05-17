7a1134ad35a8a269e69bb3d596b8f1de5705ae8b
TrujiDev <eetgrisales@gmail.com>
2024-05-17 10:28:52 -0500

    Unnecessary imports were eliminated.

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs } from 'src/config';
-import { NATS_SERVICE } from 'src/config/services';
 import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
